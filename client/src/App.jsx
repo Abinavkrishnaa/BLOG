f4a5a7d94c433e96f10a4509b3ee576b0dfbe56e
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-01-16 22:00:35 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-
-
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Projects from './pages/Projects';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+
+
+export default function SignUp() {
+  return (
+    <div>SignUp</div>
+  )
+}
