df27110372a1eb0687ad6a32efc112dba8537edb
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-01-19 21:56:39 +0530

    NavBar component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,55 @@
-
-
+import {Button, Navbar, TextInput} from 'flowbite-react';
+import { Link ,useLocation} from 'react-router-dom';
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 export default function Header() {
+  const path = useLocation().pathname;
   return (
-    <div>Header</div>
+    <Navbar className='border-b-2'>
+      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 rounded-lg text-white'>Abinav's</span>
+        Blog
+      </Link>
+      <form>
+        <TextInput 
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}  
+          className='hidden lg:inline'      
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray'>
+        <AiOutlineSearch/>
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
+        </Button>
+        <Link>
+        <Button gradientDuoTone='purpleToBlue'>
+          Sign IN
+        </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+          <Navbar.Link active={path==='/'} as={'div'}>
+            <Link to={'/'}>
+              Home
+            </Link> 
+          </Navbar.Link>
+          <Navbar.Link active={path==='/about'} as={'div'}>
+            <Link to={'/about'}>
+              About
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path==='/projects'} as={'div'}>
+            <Link to={'/projects'}>
+              Projects
+            </Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+    </Navbar>
   )
 }
 
