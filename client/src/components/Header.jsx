448c382721df7620ad7065c238adb1e5e79ab6aa
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-01-31 22:25:16 +0530

    Header comp

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,55 +1,76 @@
-import {Button, Navbar, TextInput} from 'flowbite-react';
-import { Link ,useLocation} from 'react-router-dom';
-import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className='border-b-2'>
-      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 rounded-lg text-white'>Abinav&apos;s</span>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Abinav&apos;s
+        </span>
         Blog
       </Link>
       <form>
-        <TextInput 
+        <TextInput
           type='text'
           placeholder='Search...'
-          rightIcon={AiOutlineSearch}  
-          className='hidden lg:inline'      
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray'>
-        <AiOutlineSearch/>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link>
-        <Button gradientDuoTone='purpleToBlue' outline>
-          Sign In
-        </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-          <Navbar.Link active={path==='/'} as={'div'}>
-            <Link to={'/'}>
-              Home
-            </Link> 
-          </Navbar.Link>
-          <Navbar.Link active={path==='/about'} as={'div'}>
-            <Link to={'/about'}>
-              About
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==='/projects'} as={'div'}>
-            <Link to={'/projects'}>
-              Projects
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
-
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -5,6 +5,7 @@ import { app } from '../firebase';
 import {useDispatch} from 'react-redux'
 import {signInSuccess} from '../../redux/user/userSlice'
 import { useNavigate } from 'react-router-dom';
+
 export default function OAuth() {
   const navigate = useNavigate();
   const auth = getAuth(app);
