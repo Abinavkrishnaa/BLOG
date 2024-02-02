7a1b5a8e57f63a2d159e00bcd7b13e8343f91e1e
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-02-02 21:34:07 +0530

    dark mode

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,22 +1,26 @@
-import { configureStore,combineReducers } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
-import {persistReducer, persistStore} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-// import contactsReducer from './contacts/contactsSlice';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 const rootReducer = combineReducers({
-  // key - name of the reducer (must be unique)
-  user:userReducer
-})
+  user: userReducer,
+  theme: themeReducer,
+});
+
 const persistConfig = {
-  key:'root',
+  key: 'root',
   storage,
-  version:1
-}
-const persistedReducer= persistReducer(persistConfig,rootReducer)
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-    serializableCheck:false
-  })
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
 export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,14 @@ import './index.css'
 import { store,persistor } from '../redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
   <Provider store={store}>
-    <App />
+    <ThemeProvider>
+
+      <App />
+    </ThemeProvider>
   </Provider>
   </PersistGate>
 )
