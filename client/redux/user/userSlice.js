7d142879221d93b6c9892d3746009549dd32ecbc
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-02-10 05:55:13 +0530

    profile

--- client/redux/user/userSlice.js
+++ client/redux/user/userSlice.js
@@ -1,28 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
-    currentUser: null,
-    error:null,
-    loading:false
-}
-const userSlice=createSlice({
-    name:"user",
-    "initialState":initialState,
-    reducers:{
-        signInStart:(state)=>{
-            state.loading=true;
-            state.error=null;
-        },
-        signInSuccess:(state,action) =>{
-            state.loading=false;
-            state.currentUser=action.payload;
-            state.error=null;
-        },
-        signInFailure:(state,action)=>{
-            state.loading=false;
-            state.error=action.payload;
-        }
+import { createSlice } from '@reduxjs/toolkit';
 
-    }
-})
-export const {signInStart,signInSuccess,signInFailure}=userSlice.actions;
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
+
 export default userSlice.reducer;
