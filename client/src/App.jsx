4d2e0696a73c04ce1f25745ed185b2d042b6938e
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-02-20 21:08:12 +0530

    edit post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 export default function App() {
   return (
     <BrowserRouter>
@@ -22,6 +23,8 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+
         </Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
