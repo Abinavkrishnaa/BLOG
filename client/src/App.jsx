e166da04eb6ee35cd1737c7e2653eec81af9b850
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-02-21 22:34:02 +0530

    post page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 export default function App() {
   return (
     <BrowserRouter>
@@ -29,6 +30,8 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
+        <Route path='/post/:postSlug' element={<PostPage />} />
+
       </Routes>
       <Footer/>
     </BrowserRouter>
