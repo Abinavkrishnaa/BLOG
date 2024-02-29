7ed8fc2cc259014e098d7d2ad8dd49016454adf6
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-02-29 21:31:17 +0530

    change11

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
+
 import { verifyToken } from '../utils/verifyUser.js';
 import {
     deleteUser,
