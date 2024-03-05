63812ed62dee8e8df23dd4cf8f428990414ab844
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-03-05 21:38:19 +0530

    delete comment

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,7 +3,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 import { createComment } from '../controllers/comment.controller.js';
 import {
     createComment,
-    getPostComments,likeComment,editComment
+    getPostComments,likeComment,editComment,deleteComment
   } from '../controllers/comment.controller.js';
 
 const router = express.Router();
@@ -13,6 +13,7 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
@@ -33,14 +33,14 @@ export default function Comment({ comment, onLike, onEdit }) {
 
   const handleSave = async () => {
     try {
-      const res  = await fetch(`/api/comment/editComment/${comment._id}`, {
+      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          content: editedContent
-        })
+          content: editedContent,
+        }),
       });
       if (res.ok) {
         setIsEditing(false);
@@ -49,8 +49,7 @@ export default function Comment({ comment, onLike, onEdit }) {
     } catch (error) {
       console.log(error.message);
     }
-
-  }
+  };
   return (
     <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
       <div className='flex-shrink-0 mr-3'>
@@ -71,30 +70,30 @@ export default function Comment({ comment, onLike, onEdit }) {
         </div>
         {isEditing ? (
           <>
-          <Textarea
-            className='mb-2'
-            value={editedContent}
-            onChange={(e) => setEditedContent(e.target.value)}
-          />
-          <div className="flex justify-end gap-2 text-xs">
-            <Button
-             type='button'
-             size='sm'
-             gradientDuoTone='purpleToBlue'
-              onClick={handleSave}
-            >
-              Save
-            </Button>
-            <Button
-             type='button'
-             size='sm'
-             gradientDuoTone='purpleToBlue'
-             outline
-              onClick={() => setIsEditing(false)}
-            >
-              Cancel
-            </Button>
-          </div>
+            <Textarea
+              className='mb-2'
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+            />
+            <div className='flex justify-end gap-2 text-xs'>
+              <Button
+                type='button'
+                size='sm'
+                gradientDuoTone='purpleToBlue'
+                onClick={handleSave}
+              >
+                Save
+              </Button>
+              <Button
+                type='button'
+                size='sm'
+                gradientDuoTone='purpleToBlue'
+                outline
+                onClick={() => setIsEditing(false)}
+              >
+                Cancel
+              </Button>
+            </div>
           </>
         ) : (
           <>
@@ -119,13 +118,22 @@ export default function Comment({ comment, onLike, onEdit }) {
               </p>
               {currentUser &&
                 (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button
-                    type='button'
-                    onClick={handleEdit}
-                    className='text-gray-400 hover:text-blue-500'
-                  >
-                    Edit
-                  </button>
+                  <>
+                    <button
+                      type='button'
+                      onClick={handleEdit}
+                      className='text-gray-400 hover:text-blue-500'
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => onDelete(comment._id)}
+                      className='text-gray-400 hover:text-red-500'
+                    >
+                      Delete
+                    </button>
+                  </>
                 )}
             </div>
           </>
