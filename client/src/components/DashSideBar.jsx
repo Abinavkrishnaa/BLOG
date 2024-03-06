36cf82b3bce66e0ac3bff690cf4e5f5ae81ba9e0
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-03-06 22:49:17 +0530

    dashboard overview

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -1,15 +1,21 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight,HiDocumentText,HiOutlineUserGroup ,HiAnnotation } from 'react-icons/hi';
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation, HiChartPie,
+} from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../../redux/user/userSlice';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
-  
-  const dispatch = useDispatch(); 
-  const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
   const [tab, setTab] = useState('');
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -37,6 +43,17 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+        {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
@@ -49,39 +66,43 @@ export default function DashSidebar() {
             </Sidebar.Item>
           </Link>
           {currentUser.isAdmin && (
-            <>
-            <Link to='/dashboard?tab=users'>
+            <Link to='/dashboard?tab=posts'>
               <Sidebar.Item
-                active={tab === 'users'}
-                icon={HiOutlineUserGroup}
+                active={tab === 'posts'}
+                icon={HiDocumentText}
                 as='div'
               >
-                Users
+                Posts
               </Sidebar.Item>
             </Link>
-            <Link to='/dashboard?tab=comments'>
-              <Sidebar.Item
-                active={tab === 'comments'}
-                icon={HiAnnotation}
-                as='div'
-              >
-                Comments
-              </Sidebar.Item>
-            </Link>
-          </>
           )}
           {currentUser.isAdmin && (
-            <Link to='/dashboard?tab=users'>
-              <Sidebar.Item
-                active={tab === 'users'}
-                icon={HiOutlineUserGroup}
-                as='div'
-              >
-                Users
-              </Sidebar.Item>
-            </Link>
+            <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to='/dashboard?tab=comments'>
+                <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon={HiAnnotation}
+                  as='div'
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
-          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className='cursor-pointer'
+            onClick={handleSignout}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
@@ -24,6 +25,7 @@ export default function Dashboard() {
       {tab === 'posts' && <DashPosts />}
       {tab === 'users' && <DashUsers />}
       {tab === 'comments' && <DashComments />}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
